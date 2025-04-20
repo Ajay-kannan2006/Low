54e2357520524c19b5f8b594dcad4b814db96926
Ajay-kannan2006 <ajaykannan1001200635@gmail.com>
2025-04-20 16:29:03 +0530

    added ip in convert code

--- Alimp/server.py
+++ Alimp/server.py
@@ -47,4 +47,5 @@ def convert():
 
 if __name__ == '__main__':
     print("🚀 Server is running on port 5000")
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
